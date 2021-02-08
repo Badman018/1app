a5e7f1be5cdb78913bd51d7b5809bbe2b0495db1
Matvei <motvik07@gmail.com>
2021-02-08 17:34:11 +0300

    Lesson 80 - initialize App

---
+++ src/redux/app-reducer.js
@@ -0,0 +1,30 @@
+import {setUsersDataApi} from "./auth-reducer";
+
+const INIT_APP = 'INIT_APP'
+
+let initialState = {
+    init: false
+}
+
+const appReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case INIT_APP: {
+            return {
+                ...state,
+                init: true
+            }
+        }
+        default:
+            return state
+    }
+}
+
+export const initSuccess = () => ({type: INIT_APP})
+export const initApp = () => (dispatch) => {
+    let promise = dispatch(setUsersDataApi)
+    Promise.all([promise])
+        .then(
+            dispatch(initSuccess())
+        )
+}
+export default appReducer;
